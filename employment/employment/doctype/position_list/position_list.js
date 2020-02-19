6d64204d68e1d96c9ae5859b3c9c24538d22ea3a
Rana <reh.95@hotmail.com>
2020-02-19 09:39:03 +0300

    detect in position list

--- employment/employment/doctype/position_list/position_list.js
+++ employment/employment/doctype/position_list/position_list.js
@@ -2,20 +2,6 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Position List', {
-        refresh: function (frm) {
-		if ((!frm.doc.__islocal) && (frm.doc.status == 'Arrived Externally'))
-                                   {
-			frm.add_custom_button(__('Create Employee'),
-				function () {
-					erpnext.job_offer.make_employee(frm);
-				}
-                 
-                             }
-			);
+        
 
-    }
-
-
-
-
-	}
+	});

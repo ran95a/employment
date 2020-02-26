4f08170c902d4d7ca639a513a631e8fa73714915
Rana <reh.95@hotmail.com>
2020-02-26 13:35:47 +0300

    -achange module

---
+++ employment/config/Recruitment Status Report(RSR).py
@@ -0,0 +1,30 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Recruitment Status Report(RSR)"),
+			"items": [
+				
+				{
+					"type": "doctype",
+					"name": "Vacant Position List",
+					"onboard": 1,
+				},
+				{
+					"type": "doctype",
+					"name": "Candidate List",
+					"onboard": 1,
+				},
+                {
+					"type": "doctype",
+					"name": "Employee Arrival List",
+					"onboard": 1,
+				},
+				
+				
+			]
+		},
+		
+	]

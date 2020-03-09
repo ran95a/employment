f0dbb994555eb32bdfdd0f96b266818b99d90e3c
Rana <reh.95@hotmail.com>
2020-03-09 11:27:46 +0300

    delete doc

--- employment/recruitment_status_report(rsr)/doctype/loc_cen_2/loc_cen_2.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020, frappe and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('LOC CEN 2', {
-	// refresh: function(frm) {
-
-	// }
-});
--- employment/recruitment_status_report(rsr)/doctype/rec_cen_1/rec_cen_1.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020, frappe and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('REC CEN 1', {
-	// refresh: function(frm) {
-
-	// }
-});
